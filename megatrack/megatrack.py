3fcb2731aeaf5a03fb9242338861ce595f65485a
Richard <r.stones@ucl.ac.uk>
2017-03-09 19:18:07 +0000

    Added submodule X for development. Created javascript Viewer object for nii.gz loading and rendering in browser.

--- megatrack/megatrack.py
+++ megatrack/megatrack.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from flask import render_template, json, make_response
+from flask import render_template, json, make_response, request, send_file
 import numpy as np
 #from megatrack import app
 
@@ -15,12 +15,26 @@ def index():
 def about():
     return render_template('about.html')
 
+# @app.route('/get_template')
+# def get_template():
+#     template_data = np.load('../data/compact_template_data.npz')['template_data']
+#     # may be best to put array into dict along with header data before converting to json
+#     r = make_response(json.dumps(template_data.tolist()))
+#     r.mimetype = "application/json"
+#     return r
+
 @app.route('/get_template')
 def get_template():
-    template_data = np.load('../data/compact_template_data.npz')['template_data']
-    # may be best to put array into dict along with header data before converting to json
-    r = make_response(json.dumps(template_data.tolist()))
-    r.mimetype = "application/json"
+    file_name = 'Template_T1_2mm_new.nii.gz' #'anatomical.nii.gz'
+    r = send_file('../data/'+file_name, as_attachment=True, attachment_filename=file_name, conditional=True, add_etags=True)
+    #r.last_modified = dt.datetime.fromtimestamp(os.path.getmtime(filename))
+    r.make_conditional(request)
+    return r
+
+@app.route('/get_test_map')
+def get_test_map():
+    file_name = 'mean_Cing_L_2mm.nii.gz' #'ns_map_new.nii.gz'
+    r = send_file('../data/'+file_name, as_attachment=True, attachment_filename=file_name, conditional=True, add_etags=True)
     return r
 
 @app.route('/_test_viewer')
--- megatrack/static/viewer.js
+++ megatrack/static/viewer.js
@@ -12,17 +12,85 @@ Panel.prototype = {
 /*
  * Constructor for ViewPanel class inheriting from Panel
  */
-function ViewPanel() {
+function ViewPanel(name, volume, container, orientation, vSlice, hSlice) {
 	Panel.call(this);
 	// insert a canvas tag to render slices of voxel data in a certain plane
 	// have a slider to go through slices
 	// drag cross hair to go through slices on other canvases
+	this._name = name;
+	this._volume = volume;
+	this._container = container;
+	this._orientation = orientation;
+	this._vSlice = vSlice;
+	this._hSlice = hSlice;
+	
+	this._container.append('<div id="'+this._name+'-view" class="view-panel"></div>');
+	this._container.append('<div id="'+this._name+'-slider"></div>');
+	
+	this._slice = new X.renderer2D();
+	this._slice.container = this._name+'-view';
+	this._slice.orientation = this._orientation;
+	this._slice.init();
+	this._slice.interactor.config.KEYBOARD_ENABLED = false;
+	this._slice.interactor.config.MOUSECLICKS_ENABLED = false;
+	this._slice.interactor.config.MOUSEWHEEL_ENABLED = false;
+	this._slice.interactor.init();
+	
+	this._idx = 'index' + this._orientation;
+	this._dimIdx = 'XYZ'.indexOf(this._orientation);
+	this._vIdx = 'index' + this._vSlice;
+	this._vDimIdx = 'XYZ'.indexOf(this._vSlice);
+	this._hIdx = 'index' + this._hSlice;
+	this._hDimIdx = 'XYZ'.indexOf(this._hSlice);
+	
+	this._slice.add(this._volume);
+	
+	// initialize canvas for crosshairs overlay
+	$('#'+this._name+'-view').append('<canvas id="'+this._name+'-crosshairs" class="crosshairs"></canvas>')
+	
 }
 ViewPanel.prototype = Object.create(Panel.prototype);
 ViewPanel.prototype.constructor = ViewPanel;
 
 ViewPanel.prototype.render = function() {
 	
+};
+
+ViewPanel.prototype.addSlider = function() {
+	var panel = this;
+	$('#'+this._name+'-slider').slider({
+		value: Math.floor(panel._volume[panel._idx]),
+		min: 0,
+		max: panel._volume.dimensions[panel._dimIdx]-1,
+		step: 1,
+		slide: function(event, ui) { // make this the onSlide function when its sorted
+			panel._volume[panel._idx] = ui.value;
+			// also update crosshair positions on other views
+		}
+	});
+};
+
+// actions to take on slide event, ie. move crosshairs in other views
+ViewPanel.prototype.onSlide = function() {
+	
+}
+
+ViewPanel.prototype.drawCrosshairs = function() {
+	var canvas = $('#'+this._name+'-crosshairs').get(0);
+	var canvasWidth = canvas.width;
+	var canvasHeight = canvas.height;
+	var ctx = canvas.getContext('2d');
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = "rgba(0,255,0,0.7)";
+	// vertical slice
+	var vSlicePos = canvasWidth * (this._volume[this._vIdx] / this._volume.dimensions[this._vDimIdx]);
+	ctx.moveTo(vSlicePos, 0);
+	ctx.lineTo(vSlicePos, canvasHeight);
+	// horizontal slice
+	var hSlicePos = canvasHeight * (this._volume[this._hIdx] / this._volume.dimensions[this._hDimIdx]);
+	ctx.moveTo(0, hSlicePos);
+	ctx.lineTo(canvasWidth, hSlicePos);
+	ctx.stroke();
 }
 
 /*
@@ -39,84 +107,63 @@ QueryPanel.prototype.constructor = QueryPanel;
  * Constructor for Viewer class
  */
 function Viewer(elementId) {
-	// insert Panel objects
+	
 	this.elementId = elementId;
 	var container = $('#'+this.elementId);
-	container.append('<div id="coronal-view-panel"></div>');
-	container.append('<div id="sagittal-view-panel"></div>');
-	container.append('<div id="transverse-view-panel"></div>');
+	container.append('<div id="coronal-panel"></div>');
+	container.append('<div id="sagittal-panel"></div>');
+	container.append('<div id="axial-panel"></div>');
 	container.append('<div id="query-panel"></div>');
 	$('#'+this.elementId+' div').addClass('viewer-panel');
-	$('[id*="view-panel"]').addClass('view-panel'); // attribute contains selector
-	
-	var currentCoronalSlide = 45;
-	
-	// get template data via AJAX request
-	// on callback call render function to display initial data
-	$.ajax({
-		url: $SCRIPT_ROOT + '/get_template',
-		method: 'GET',
-		dataType: 'json',
-		success: function(data) {
-			// set property of Viewer instance to hold template data
-			console.log('data received');
-			// coronal slice
-			var coronalViewPanel = $('#coronal-view-panel')
-			for (var i = 0; i < data.length; i++) {
-				var canvasId = 'coronal-view-panel-slice'+i;
-				coronalViewPanel.append('<canvas id="'+canvasId+'" style="width:150px; height:150px; display: none;"></canvas>');
-				// render each slice on current canvas
-				var canvas = document.getElementById(canvasId);
-				var ctx = canvas.getContext("2d");
-				var xLen = data[0].length;
-				var yLen = data[0][0].length
-				var imgData = ctx.createImageData(xLen, yLen);
-				/*for (var j = 0; j < imgData.data.length; j+=4) {
-					imgData.data[j+0] = imgData.data[j+1] = imgData.data[j+2] = 255; //data[i][j];
-					imgData.data[j+3] = 255;
-				}*/
-				for (var j = 0; j < yLen; j++) {
-					for (var k = 0; k < xLen; k++) {
-						var idx = 4 * (j * xLen + k);
-						//console.log(k + '    ' + j)
-						imgData.data[idx+0] = imgData.data[idx+1] = imgData.data[idx+2] = data[i][k][j];
-						imgData.data[idx+3] = 255;
-					}
-				}
-				ctx.putImageData(imgData, 50, 50);
-			}
-			// make initial slice visible
-			$('#coronal-view-panel-slice'+currentCoronalSlide).show();
-			coronalViewPanel.append('<div id="slider"></div>');
-			$('#slider').slider({
-				value: 45,
-				min: 0,
-				max: 90,
-				step: 1,
-				slide: function(event, ui) {
-					$('#coronal-view-panel-slice'+currentCoronalSlide).hide();
-					$('#coronal-view-panel-slice'+ui.value).show();
-					currentCoronalSlide = ui.value;
-				}
-				
-			});
-			// stop loading gif
-		}
+
+	volume = new X.volume();
+	volume.file = '/get_template?.nii.gz';
+	volume.labelmap.file = '/get_test_map?.nii.gz'
+	var cmapShades = 100;
+	var cmap = Colormaps({
+		colormap: [{"index":0, "rgb":[0,0,0,0]}, {"index":0.01, "rgb":[100,0,0,0.6]}, {"index":0.25, "rgb":[100,0,0,0.8]}, {"index":0.55, "rgb":[150,0,0,0.8]}, {"index":0.65, "rgb":[200,0,0,0.9]}, {"index":0.75, "rgb":[250,0,0,1]}, {"index":0.85, "rgb":[255,0,0,1]}, {"index":1, "rgb":[255,255,0,1]}],
+		alpha: [0,1],
+		nshades: cmapShades,
+		format: 'rgbaString'
 	});
-	// display loading gif while waiting for callback
+	volume.labelmap.colormap = function(normpixval) {
+		var rgbaString = cmap[Math.floor((cmapShades-1)*normpixval)];
+		rgbaString = rgbaString.replace(/[^\d,.]/g, '').split(',');
+		var rgba = [];
+		for (var i = 0; i<3; i++) rgba.push(parseInt(rgbaString[i], 10));
+		rgba.push(255*parseFloat(rgbaString[3]));
+		return rgba
+	};	
 	
+	this._viewPanels = {};
+	this._viewPanels['sagittal'] = new ViewPanel('sagittal', volume, $('#sagittal-panel'), 'X', 'Y', 'Z');
+	this._viewPanels['sagittal']._slice.render();
 	
+	this._viewPanels['sagittal']._slice.onShowtime = function() {
+		
+		viewer._viewPanels['coronal'] = new ViewPanel('coronal', volume, $('#coronal-panel'), 'Y', 'X', 'Z');
+		viewer._viewPanels['coronal']._slice.render();
+		viewer._viewPanels['axial'] = new ViewPanel('axial', volume, $('#axial-panel'), 'Z', 'X', 'Y');
+		viewer._viewPanels['axial']._slice.render();
+		
+		viewer._viewPanels['sagittal'].addSlider();
+		viewer._viewPanels['coronal'].addSlider();
+		viewer._viewPanels['axial'].addSlider();
+		viewer._viewPanels['sagittal'].drawCrosshairs();
+		viewer._viewPanels['coronal'].drawCrosshairs();
+		viewer._viewPanels['axial'].drawCrosshairs();
+	}
 	
 };
 
 Viewer.prototype = {
 		constructor: Viewer,
-		display: function() {
+		onSlide: function() {
 			
 		}
 }
 
 $(document).ready(function() {
 	// insert Viewer into div with id="viewer"
-	var viewer = new Viewer("viewer");
+	viewer = new Viewer("viewer");
 });
