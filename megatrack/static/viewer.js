98acaf78a711ff31ffe59a5c7200473c848816f3
Richard <r.stones@ucl.ac.uk>
2017-03-10 17:57:26 +0000

    Added event driven coordination of sliders and crosshairs in Viewer.

--- megatrack/static/viewer.js
+++ megatrack/static/viewer.js
@@ -1,132 +1,133 @@
-/*
- * Constructor for Panel class
+/**
+ * Create a X.renderer2D to display slice in single plane
+ * @constructor
+ * @param {string} plane The neurological name of slicing plane eg. sagittal, coronal or axial.
+ * @param {X.volume} volume
+ * @param {JQuery DOM object} container The container for the View.
+ * @param {string} orientation The label for the slice orientation eg. X,Y,Z.
+ * @param {string} vSlice The orientation for slices vertical relative to orientation of View.
+ * @param {string} hSlice The orientation for slices horizontal relative to orientation of View.
  */
-function Panel() {
-	// insert div with certain dimensions to hold either an image or a form
-};
-
-Panel.prototype = {
-		constructor: Panel
-}
-
-/*
- * Constructor for ViewPanel class inheriting from Panel
- */
-function ViewPanel(name, volume, container, orientation, vSlice, hSlice) {
-	Panel.call(this);
-	// insert a canvas tag to render slices of voxel data in a certain plane
-	// have a slider to go through slices
-	// drag cross hair to go through slices on other canvases
-	this._name = name;
+function View(plane, volume, container, orientation, vSlice, hSlice) {
+	
+	this._plane = plane;
 	this._volume = volume;
 	this._container = container;
 	this._orientation = orientation;
 	this._vSlice = vSlice;
 	this._hSlice = hSlice;
 	
-	this._container.append('<div id="'+this._name+'-view" class="view-panel"></div>');
-	this._container.append('<div id="'+this._name+'-slider"></div>');
+	this._container.append('<div id="'+this._plane+'-view" class="view"></div>');
+	this._container.append('<div id="'+this._plane+'-slider"></div>');
 	
-	this._slice = new X.renderer2D();
-	this._slice.container = this._name+'-view';
-	this._slice.orientation = this._orientation;
-	this._slice.init();
-	this._slice.interactor.config.KEYBOARD_ENABLED = false;
-	this._slice.interactor.config.MOUSECLICKS_ENABLED = false;
-	this._slice.interactor.config.MOUSEWHEEL_ENABLED = false;
-	this._slice.interactor.init();
+	this._view = new X.renderer2D();
+	this._view.radiological = false;
+	this._view.container = this._plane+'-view';
+	this._view.orientation = this._orientation;
+	this._view.init();
+	this._view.interactor.config.KEYBOARD_ENABLED = false;
+	this._view.interactor.config.MOUSECLICKS_ENABLED = false;
+	this._view.interactor.config.MOUSEWHEEL_ENABLED = false;
+	this._view.interactor.init();
 	
 	this._idx = 'index' + this._orientation;
-	this._dimIdx = 'XYZ'.indexOf(this._orientation);
+	this._dimIdx = 'XYZ'.indexOf(this._orientation); // just get dim here and use it where its needed
 	this._vIdx = 'index' + this._vSlice;
-	this._vDimIdx = 'XYZ'.indexOf(this._vSlice);
+	this._vDimIdx = 'XYZ'.indexOf(this._vSlice); // same here
 	this._hIdx = 'index' + this._hSlice;
-	this._hDimIdx = 'XYZ'.indexOf(this._hSlice);
+	this._hDimIdx = 'XYZ'.indexOf(this._hSlice); // same here
 	
-	this._slice.add(this._volume);
+	this._view.add(this._volume);
 	
 	// initialize canvas for crosshairs overlay
-	$('#'+this._name+'-view').append('<canvas id="'+this._name+'-crosshairs" class="crosshairs"></canvas>')
-	
+	$('#'+this._plane+'-view').append('<canvas id="'+this._plane+'-crosshairs" class="crosshairs"></canvas>')
 }
-ViewPanel.prototype = Object.create(Panel.prototype);
-ViewPanel.prototype.constructor = ViewPanel;
+View.prototype.constructor = View;
 
-ViewPanel.prototype.render = function() {
-	
-};
-
-ViewPanel.prototype.addSlider = function() {
-	var panel = this;
-	$('#'+this._name+'-slider').slider({
-		value: Math.floor(panel._volume[panel._idx]),
+View.prototype.addSlider = function() {
+	var view = this;
+	$('#'+this._plane+'-slider').slider({
+		value: Math.floor(view._volume[view._idx]),
 		min: 0,
-		max: panel._volume.dimensions[panel._dimIdx]-1,
+		max: view._volume.dimensions[view._dimIdx]-1,
 		step: 1,
-		slide: function(event, ui) { // make this the onSlide function when its sorted
-			panel._volume[panel._idx] = ui.value;
-			// also update crosshair positions on other views
+		slide: function(event, ui) {
+			$('#viewer').trigger('view:slide', [view._plane, ui.value]);
 		}
 	});
 };
 
-// actions to take on slide event, ie. move crosshairs in other views
-ViewPanel.prototype.onSlide = function() {
-	
-}
-
-ViewPanel.prototype.drawCrosshairs = function() {
-	var canvas = $('#'+this._name+'-crosshairs').get(0);
-	var canvasWidth = canvas.width;
-	var canvasHeight = canvas.height;
-	var ctx = canvas.getContext('2d');
-	ctx.lineWidth = 1;
-	ctx.strokeStyle = "rgba(0,255,0,0.7)";
-	// vertical slice
-	var vSlicePos = canvasWidth * (this._volume[this._vIdx] / this._volume.dimensions[this._vDimIdx]);
-	ctx.moveTo(vSlicePos, 0);
-	ctx.lineTo(vSlicePos, canvasHeight);
-	// horizontal slice
-	var hSlicePos = canvasHeight * (this._volume[this._hIdx] / this._volume.dimensions[this._hDimIdx]);
-	ctx.moveTo(0, hSlicePos);
-	ctx.lineTo(canvasWidth, hSlicePos);
-	ctx.stroke();
+View.prototype.initSlicingOverlay = function() {
+	var canvas = $('#'+this._plane+'-crosshairs').get(0);
+	var viewContainer = $('#'+this._plane+'-crosshairs').parent().get(0);
+	canvas.width = 250; // bad to hard code width/height here
+	canvas.height = 250;
+	var view = this;
+	canvas.onclick = function(event) {
+		var x = event.pageX - viewContainer.offsetLeft;
+		var y = event.pageY - viewContainer.offsetTop;
+		//view._volume[view._vIdx] = Math.round(view._volume.dimensions[view._vDimIdx] * (x / canvas.width));
+		//view._volume[view._hIdx] = Math.round(view._volume.dimensions[view._hDimIdx] * (y / canvas.height));
+		//view.drawCrosshairs();
+		$('#viewer').trigger('view:click', [view._plane, x, y, canvas.width, canvas.height]);
+	};
+	this.drawCrosshairs();
 }
 
 /*
- * Constructor for QueryPanel class inheriting from Panel
+ * Horizontal line is stretched because canvas shape is altered by css which scales it
  */
-function QueryPanel() {
-	Panel.call(this);
-	// insert widgets from JQuery UI to submit to server via a form
+View.prototype.drawCrosshairs = function() {
+	var canvas = $('#'+this._plane+'-crosshairs').get(0);
+	var vSlicePos = canvas.width * (this._volume[this._vIdx] / this._volume.dimensions[this._vDimIdx]);
+	var hSlicePos = canvas.height * (this._volume[this._hIdx] / this._volume.dimensions[this._hDimIdx]);
+	var ctx = canvas.getContext('2d');
+	ctx.beginPath();
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.lineWidth = 2;
+	ctx.strokeStyle = "rgba(0,220,0,0.5)";
+	// vertical slice
+	ctx.moveTo(vSlicePos, 0);
+	ctx.lineTo(vSlicePos, canvas.height);
+	// horizontal slice
+	ctx.moveTo(0, hSlicePos);
+	ctx.lineTo(canvas.width, hSlicePos);
+	ctx.stroke();
+	
+	//ctx.rect(x - 6, y - 6, 10, 10);
+	//ctx.stroke();
 }
-QueryPanel.prototype = Object.create(Panel.prototype);
-QueryPanel.prototype.constructor = QueryPanel;
 
-/*
- * Constructor for Viewer class
+/**
+ * Create object to contain multiple X.renderer2D objects to display different slices of MRI data
+ * @constructor
+ * @param {string} elementId ID of container for Viewer
  */
 function Viewer(elementId) {
 	
-	this.elementId = elementId;
-	var container = $('#'+this.elementId);
+	this._elementId = elementId;
+	var container = $('#'+this._elementId);
 	container.append('<div id="coronal-panel"></div>');
 	container.append('<div id="sagittal-panel"></div>');
 	container.append('<div id="axial-panel"></div>');
 	container.append('<div id="query-panel"></div>');
-	$('#'+this.elementId+' div').addClass('viewer-panel');
+	$('#'+this._elementId+' div').addClass('viewer-panel');
 
-	volume = new X.volume();
-	volume.file = '/get_template?.nii.gz';
-	volume.labelmap.file = '/get_test_map?.nii.gz'
+	this._volume = new X.volume(); // is it a good idea for volume to be global?
+	this._volume.file = '/get_template?.nii.gz'; // should these addresses be a bit more hidden for security? see neurosynth
+	this._volume.labelmap.file = '/get_test_map?.nii.gz';
+	this._volume.transform = new X.transform(); // switch to neurological convention
+	this._volume.transform.flipY();
 	var cmapShades = 100;
 	var cmap = Colormaps({
-		colormap: [{"index":0, "rgb":[0,0,0,0]}, {"index":0.01, "rgb":[100,0,0,0.6]}, {"index":0.25, "rgb":[100,0,0,0.8]}, {"index":0.55, "rgb":[150,0,0,0.8]}, {"index":0.65, "rgb":[200,0,0,0.9]}, {"index":0.75, "rgb":[250,0,0,1]}, {"index":0.85, "rgb":[255,0,0,1]}, {"index":1, "rgb":[255,255,0,1]}],
+		colormap: [{"index":0, "rgb":[0,0,0,0]}, {"index":0.01, "rgb":[100,0,0,0.7]}, {"index":0.25, "rgb":[100,0,0,0.8]},
+			       {"index":0.55, "rgb":[150,0,0,0.9]}, {"index":0.65, "rgb":[200,0,0,1]}, {"index":0.75, "rgb":[250,0,0,1]},
+			       {"index":0.85, "rgb":[255,0,0,1]}, {"index":1, "rgb":[255,255,0,1]}],
 		alpha: [0,1],
 		nshades: cmapShades,
 		format: 'rgbaString'
 	});
-	volume.labelmap.colormap = function(normpixval) {
+	this._volume.labelmap.colormap = function(normpixval) {
 		var rgbaString = cmap[Math.floor((cmapShades-1)*normpixval)];
 		rgbaString = rgbaString.replace(/[^\d,.]/g, '').split(',');
 		var rgba = [];
@@ -135,24 +136,41 @@ function Viewer(elementId) {
 		return rgba
 	};	
 	
-	this._viewPanels = {};
-	this._viewPanels['sagittal'] = new ViewPanel('sagittal', volume, $('#sagittal-panel'), 'X', 'Y', 'Z');
-	this._viewPanels['sagittal']._slice.render();
+	this._views = {};
+	this._views['sagittal'] = new View('sagittal', this._volume, $('#sagittal-panel'), 'X', 'Y', 'Z');
+	this._views['sagittal']._view.render();
+	this._views['sagittal']._view.onShowtime = function() {
+		
+		viewer._views['coronal'] = new View('coronal', viewer._volume, $('#coronal-panel'), 'Y', 'X', 'Z');
+		viewer._views['coronal']._view.render();
+		viewer._views['axial'] = new View('axial', viewer._volume, $('#axial-panel'), 'Z', 'X', 'Y');
+		viewer._views['axial']._view.render();
+		
+		// initialize sliders and crosshairs
+		for (var view in viewer._views) {
+			viewer._views[view].addSlider();
+			viewer._views[view].initSlicingOverlay();
+		}
+	};
 	
-	this._viewPanels['sagittal']._slice.onShowtime = function() {
-		
-		viewer._viewPanels['coronal'] = new ViewPanel('coronal', volume, $('#coronal-panel'), 'Y', 'X', 'Z');
-		viewer._viewPanels['coronal']._slice.render();
-		viewer._viewPanels['axial'] = new ViewPanel('axial', volume, $('#axial-panel'), 'Z', 'X', 'Y');
-		viewer._viewPanels['axial']._slice.render();
-		
-		viewer._viewPanels['sagittal'].addSlider();
-		viewer._viewPanels['coronal'].addSlider();
-		viewer._viewPanels['axial'].addSlider();
-		viewer._viewPanels['sagittal'].drawCrosshairs();
-		viewer._viewPanels['coronal'].drawCrosshairs();
-		viewer._viewPanels['axial'].drawCrosshairs();
-	}
+	$('#viewer').on('view:slide', function(event, plane, sliderVal) {
+		//update volume for activated View
+		viewer._volume[viewer._views[plane]._idx] = sliderVal;
+		// update slice lines on other Views
+		for (var view in viewer._views) {
+			viewer._views[view].drawCrosshairs();
+		}
+	});
+	
+	$('#viewer').on('view:click', function(event, plane, x, y, canvasWidth, canvasHeight) {
+		// update volume for other Views
+		viewer._volume[viewer._views[plane]._vIdx] = Math.round(viewer._volume.dimensions[viewer._views[plane]._vDimIdx] * (x / canvasWidth));
+		viewer._volume[viewer._views[plane]._hIdx] = Math.round(viewer._volume.dimensions[viewer._views[plane]._hDimIdx] * (y / canvasHeight));
+		// update slice lines on all Views
+		for (var view in viewer._views) {
+			viewer._views[view].drawCrosshairs();
+		}
+	});
 	
 };
 
@@ -164,6 +182,7 @@ Viewer.prototype = {
 }
 
 $(document).ready(function() {
-	// insert Viewer into div with id="viewer"
 	viewer = new Viewer("viewer");
+	// is it a good idea for viewer to be global? it is needed for onShowtime function
+	// may want to wrap it in a namespace eventually
 });
