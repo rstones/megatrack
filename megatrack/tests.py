a39c301111ba561023bb3d7a67b0bd2d48003aaf
Richard <r.stones@ucl.ac.uk>
2017-04-06 18:49:02 +0100

    Fixed test set up

--- megatrack/tests.py
+++ megatrack/tests.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import unittest
 from flask_testing import TestCase
-from .models import db, AlchemyEncoder
+from .models import db, AlchemyEncoder, Tract
 from .views import megatrack
         
 class MegatrackTestCase(TestCase):
@@ -22,22 +22,32 @@ class MegatrackTestCase(TestCase):
         db.drop_all()
     
     def test_index(self):
-        resp = self.app.get('/')
+        resp = self.client.get('/')
         assert b'Home page' in resp.get_data()
     
     def test_about(self):
-        resp = self.app.get('/about')
+        resp = self.client.get('/about')
         assert b'About page' in resp.get_data()
     
     def test_get_template(self):
-        resp = self.app.get('/get_template')
+        resp = self.client.get('/get_template')
         assert resp.mimetype == 'application/octet-stream'
         assert resp.headers.get('Content-Disposition') == 'attachment; filename=Template_T1_2mm_new.nii.gz'
         assert resp.headers.get('Content-Length') == '1439379'
         
     def test_populate_tract_select(self):
-        resp = self.app.get('/tract_select')
+        # insert test tract
+        code = 'CINGL'; name = 'Left Cingulum'; file_path ='/CINGL'
+        cingL = Tract(code=code, name=name, file_path=file_path)
+        db.session.add(cingL)
+        db.session.commit()
+        # get response
+        resp = self.client.get('/tract_select')
+        # test response
         assert resp.mimetype == 'application/json'
+        assert bytes('"code": "'+code+'"', 'utf-8') in resp.get_data()
+        assert bytes('"name": "'+name+'"', 'utf-8') in resp.get_data()
+        assert bytes('"file_path": "'+file_path+'"', 'utf-8') not in resp.get_data() # we don't want to expose file paths
 
 if __name__ == '__main__':
     unittest.main()
