fdcf44e84f6979a63ea5db835fbd30bcd099860e
Richard <r.stones@ucl.ac.uk>
2017-12-01 17:14:42 +0000

    Added tests for database_utils and tidied some code

--- megatrack/database_utils.py
+++ megatrack/database_utils.py
@@ -7,9 +7,13 @@ from megatrack.models import Tract, Subject, Dataset, DatasetTracts, SubjectTrac
 import numpy as np
 
 def get_dataset_select_info():
+    '''Get all the datasets for select menu'''
     return Dataset.query.all()
 
 def get_tract_select_info():
+    '''Get tracts for select menu, grouped by the those attached to at least one dataset and those unattached.
+    Ensures that a link is defined between each tract and the datasets it is available for in order to be included
+    in the select menu.'''
     result = {}
     ignored_tracts = []
     
@@ -31,6 +35,9 @@ def get_tract_select_info():
 
 
 def construct_subject_query_filter(dataset_constraints):
+    ''' Constructs a list of SQL Alchemy BinaryExpressions for queries on subjects within a certain dataset.
+    dataset_constraints is a dict which defines the type of constraint. The possible constraints 
+    for each dataset are defined in Dataset.query_params field.'''
     dataset_filter = []
     for constraint_field in dataset_constraints:
         constraint_info = dataset_constraints[constraint_field]
@@ -47,9 +54,11 @@ def construct_subject_query_filter(dataset_constraints):
     return dataset_filter
 
 def get_tract(tract_code):
+    '''Get a single tract based on the tract code'''
     return Tract.query.filter(Tract.code == tract_code).first()
 
 def subjects_per_dataset(request_query):
+    '''The number of subjects returned per dataset for the given query'''
     result = {"dataset":{}}
     for key in request_query:
         dataset_filter = construct_subject_query_filter(request_query[key])
@@ -59,6 +68,7 @@ def subjects_per_dataset(request_query):
     return result
 
 def subject_id_dataset_file_path(request_query):
+    '''Returns a list of tuples containing subject id and associated dataset code for the given query'''
     ids_file_paths = []
     for key in request_query:
         dataset_filter = construct_subject_query_filter(request_query[key])
@@ -66,22 +76,23 @@ def subject_id_dataset_file_path(request_query):
         ids_file_paths += Subject.query.join(Dataset).with_entities(Subject.subject_id, Dataset.file_path).filter(*dataset_filter).all()
     return ids_file_paths
 
-def construct_subject_file_paths(request_query, data_file_path):
-    results = {"dataset":{}}
-    full_file_paths = []
-        
-    for key in request_query:
-        dataset_filter = construct_subject_query_filter(request_query[key])
-        dataset_filter.append(Subject.dataset_code == key)
-        subject_ids = Subject.query.with_entities(Subject.subject_id).filter(*dataset_filter).all()
-        dataset_file_path = Dataset.query.with_entities(Dataset.file_path).filter(Dataset.file_path == key).first()[0]            
-        for id in subject_ids:
-            full_file_paths.append(data_file_path + dataset_file_path + '/full_brain_maps/mni/' + id[0])
-        results['dataset'][key] = len(subject_ids)
-        
-    return results, full_file_paths
+# def construct_subject_file_paths(request_query, data_file_path):
+#     results = {"dataset":{}}
+#     full_file_paths = []
+#         
+#     for key in request_query:
+#         dataset_filter = construct_subject_query_filter(request_query[key])
+#         dataset_filter.append(Subject.dataset_code == key)
+#         subject_ids = Subject.query.with_entities(Subject.subject_id).filter(*dataset_filter).all()
+#         dataset_file_path = Dataset.query.with_entities(Dataset.file_path).filter(Dataset.file_path == key).first()[0]            
+#         for id in subject_ids:
+#             full_file_paths.append(data_file_path + dataset_file_path + '/full_brain_maps/mni/' + id[0])
+#         results['dataset'][key] = len(subject_ids)
+#         
+#     return results, full_file_paths
 
 def subject_tract_metrics(request_query, tract_code):
+    '''Get the subject tract metrics for the subjects returned from the given query and the given tract code.'''
     subject_ids = []
     for key in request_query:
         dataset_filter = construct_subject_query_filter(request_query[key])
