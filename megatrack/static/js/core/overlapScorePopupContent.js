239426f16778b2175c4a88b16d820ee16fc9d4e6
Richard <r.stones@ucl.ac.uk>
2018-09-07 17:26:52 +0100

    Added content for overlap score popup

--- megatrack/static/js/core/overlapScorePopupContent.js
+++ megatrack/static/js/core/overlapScorePopupContent.js
@@ -3,5 +3,14 @@ var mgtrk = mgtrk || {};
 mgtrk.popupContent = mgtrk.popupContent || {};
 
 mgtrk.popupContent.overlapScore = (contentWrapperId) => {
-    $(`#${contentWrapperId}`).append(`There will be some info here eventually about how the overlap score was calculated.`);
+    $(`#${contentWrapperId}`).append(`<div id="overlap-score-popup-title">Overlap score</div>
+                                      <div id="overlap-score-popup-description">
+                                          The overlap score is calculated as follows:
+                                          <ul>
+                                              <li>The voxels of the tract probability map which are greater than or
+                                              equal to the provided threshold are taken.</li>
+                                              <li>The overlap score is then the percentage of these voxels which overlap
+                                              with the lesion binary mask.</li>
+                                          </ul>
+                                      </div>`);
 };
