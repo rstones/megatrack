1c80320d816348a87ca02d0f50568dc56cd69587
Richard <r.stones@ucl.ac.uk>
2018-09-07 17:38:36 +0100

    Added content to tract disconnect popup

--- megatrack/static/js/core/disconnectPopupContent.js
+++ megatrack/static/js/core/disconnectPopupContent.js
@@ -3,5 +3,16 @@ var mgtrk = mgtrk || {};
 mgtrk.popupContent = mgtrk.popupContent || {};
 
 mgtrk.popupContent.disconnect = (contentWrapperId) => {
-    $(`#${contentWrapperId}`).append(`This will eventually tell you something about how the disconnection is calculated.`);
+    $(`#${contentWrapperId}`).append(`<div id="disconnect-popup-title">Tract disconnection</div>
+                                      <div id="disconnect-popup-description">
+                                          The tract disconnection is calculated as follows:
+                                          <ul>
+                                              <li>Three quantities are obtained for each subject in the selected 
+                                              demographic. The total number of streamlines in the trk file for the
+                                              specified tract, the number of streamlines that intersect the lesion
+                                              and the percentage of these disconnected streamlines.</li>
+                                              <li>Each of these quantities is averaged over the selected demographic
+                                              to give the tract disconnection results.</li>
+                                          </ul>
+                                      </div>`);
 };
