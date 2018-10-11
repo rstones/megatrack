c727f08d909d46d8a7cb4b5f463571b07fd7cc4f
Richard <r.stones@ucl.ac.uk>
2018-10-11 09:54:21 +0100

    Sorted out logger formatter

--- run.py
+++ run.py
@@ -5,13 +5,12 @@ from logging.handlers import TimedRotatingFileHandler
 # configure logging, switches over to new log file every midnight
 if not application.debug:
     handler = TimedRotatingFileHandler(application.config['LOG_FILE_PATH'], when='midnight')
-    #handler.setFormatter(logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s'))
-    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s %(message)s'))
+    handler.setFormatter(logging.Formatter('%(asctime)s [pid: %(process)d] %(levelname)-4s %(funcName)s: %(message)s'))
     application.logger.addHandler(handler)
     application.logger.setLevel(logging.INFO)
 
 if __name__=='__main__':
-    application.debug = application.config['FLASK_DEBUG']    
+    application.debug = application.config['FLASK_DEBUG']
     application.logger.info('Starting application...')
     db.create_all(app=application)
     application.run()
