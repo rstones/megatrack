f5a0e3fb011c062fcd30a65a4c3b9fc1d1f967a0
Richard <r.stones@ucl.ac.uk>
2018-10-12 12:04:57 +0100

    Split tract disconnection explanation into paragraphs

--- megatrack/static/js/core/disconnectPopupContent.js
+++ megatrack/static/js/core/disconnectPopupContent.js
@@ -6,10 +6,14 @@ mgtrk.popupContent.disconnect = (contentWrapperId) => {
     $(`#${contentWrapperId}`).append(`<div id="disconnect-popup-title">Tract disconnection</div>
                                       <div id="disconnect-popup-description">
                                           The tract disconnection provides a more in depth analysis of the lesion
-                                          overlap at the streamline level. Three quantities are obtained for each
-                                          subject in the currently selected query: the total number of streamlines
-                                          in the tract, the number of streamlines intersecting the lesion and the
-                                          percentage of these disconnected streamlines. The results shown are the
-                                          averages of these quantities across the selected demographic.
+                                          overlap at the streamline level.
+                                          <br><br>
+                                          Three quantities are obtained for each subject in the currently selected
+                                          query: the total number of streamlines in the tract, the number of
+                                          streamlines intersecting the lesion and the percentage of these disconnected
+                                          streamlines.
+                                          <br><br>
+                                          The results shown are the averages of these quantities across the selected
+                                          demographic.
                                       </div>`);
 };
