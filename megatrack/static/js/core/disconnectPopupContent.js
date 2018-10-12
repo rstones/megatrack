f2db37464201f2b0b4ea34252620395feda0238a
Richard <r.stones@ucl.ac.uk>
2018-10-12 11:39:22 +0100

    Updated tract disconnection explanation

--- megatrack/static/js/core/disconnectPopupContent.js
+++ megatrack/static/js/core/disconnectPopupContent.js
@@ -5,14 +5,11 @@ mgtrk.popupContent = mgtrk.popupContent || {};
 mgtrk.popupContent.disconnect = (contentWrapperId) => {
     $(`#${contentWrapperId}`).append(`<div id="disconnect-popup-title">Tract disconnection</div>
                                       <div id="disconnect-popup-description">
-                                          The tract disconnection is calculated as follows:
-                                          <ul>
-                                              <li>Three quantities are obtained for each subject in the selected 
-                                              demographic. The total number of streamlines in the trk file for the
-                                              specified tract, the number of streamlines that intersect the lesion
-                                              and the percentage of these disconnected streamlines.</li>
-                                              <li>Each of these quantities is averaged over the selected demographic
-                                              to give the tract disconnection results.</li>
-                                          </ul>
+                                          The tract disconnection provides a more in depth analysis of the lesion
+                                          overlap at the streamline level. Three quantities are obtained for each
+                                          subject in the currently selected query: the total number of streamlines
+                                          in the tract, the number of streamlines intersecting the lesion and the
+                                          percentage of these disconnected streamlines. The results shown are the
+                                          averages of these quantities across the selected demographic.
                                       </div>`);
 };
